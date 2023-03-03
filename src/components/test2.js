2343432085676660077ec6c1659b7857567952a9
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-03 16:33:43 +0100

    have to add style

--- src/components/test2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Test_2() {
-  return (
-    <div>test2222</div>
-  )
-}
--- src/components/test3.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Test_3() {
-  return (
-    <div>test3333</div>
-  )
-}
